f21170966320a918767871b85013047f01c8a89c
jesoukup <33041461+jesoukup@users.noreply.github.com>
2018-03-19 17:30:42 -0500

    Add files via upload

--- AnalysisTest.java
+++ AnalysisTest.java
@@ -3,8 +3,17 @@ public class AnalysisTest {
 
                 // TODO Add code for checking command line arguments
                 
-                PerformanceAnalysisHash ana = new PerformanceAnalysisHash("data\\IntegerLarge.txt");
+                PerformanceAnalysisHash ana = new PerformanceAnalysisHash("data\\IntegerSmall.txt");
                 ana.compareDataStructures();
                 ana.printReport();
+                PerformanceAnalysisHash ana2 = new PerformanceAnalysisHash("data\\IntegerLarge.txt");
+                ana2.compareDataStructures();
+                ana2.printReport();
+                PerformanceAnalysisHash ana3 = new PerformanceAnalysisHash("data\\StringSmall.txt");
+                ana3.compareDataStructures();
+                ana3.printReport();
+                PerformanceAnalysisHash ana4 = new PerformanceAnalysisHash("data\\StringLarge.txt");
+                ana4.compareDataStructures();
+                ana4.printReport();
             }
 }
